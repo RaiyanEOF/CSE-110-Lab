6b8e841ca01943928eccf8c079efc63f68866369
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-21 00:32:00 +0600

    Create Task_03.java

---
+++ Lab 02/Task_03.java
@@ -0,0 +1,11 @@
+public class Task03{
+    public static void main(String[] args){
+        int Radius = 4;
+        Double Pie = 3.1416;
+        Double Radius2 = Math.pow(4, 2);
+        Double Circumference = 2*Radius*Pie;
+        Double Area = Radius2*Pie;
+        System.out.println("The circumference is " + Circumference);
+        System.out.println("The area is " + Area);
+    }
+}
