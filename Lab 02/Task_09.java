34f394f7ac4576af3762b99e3d0fca425af56a73
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-21 00:34:38 +0600

    Create Task_09.java

---
+++ Lab 02/Task_09.java
@@ -0,0 +1,26 @@
+public class Task09{
+    public static void main(String[] args){
+      int i = 6;
+      int t1 = i*1;
+      int t2 = i*2;
+      int t3 = i*3;
+      int t4 = i*4;
+      int t5 = i*5;
+      int t6 = i*6;
+      int t7 = i*7;
+      int t8 = i*8;
+      int t9 = i*9;
+      int t10 = i*10;
+      System.out.println( i + "*1=" + t1);
+      System.out.println( i + "*2=" + t2);
+      System.out.println( i + "*3=" + t3);
+      System.out.println( i + "*4=" + t4);
+      System.out.println( i + "*5=" + t5);
+      System.out.println( i + "*6=" + t6);
+      System.out.println( i + "*7=" + t7);
+      System.out.println( i + "*8=" + t8);
+      System.out.println( i + "*9=" + t9);
+      System.out.println( i + "*10=" + t10);
+          
+    }
+}
