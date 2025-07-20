c9b16ab00a4532d3e8871b8ceb2381fe9c73cd00
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-21 00:30:46 +0600

    Create Task_01.java

---
+++ Lab 02/Task_01.java
@@ -0,0 +1,27 @@
+public class Task01{
+    public static void main(String[] args){
+      //(a)
+        int hungry; // Not a legal Identifier
+      //(b)
+        int  2AB; // Using multiple character in variable declaration
+      //(c)
+        int  312.2; // Using double datatype in Integer datatype
+      //(d)
+        int MOBILE // Not an Integer value
+      //(e)
+        int “Ans”; // Using " " in variable declaration
+      //(f)
+        int $30; // Using character with integer value
+      //(g)
+        int  Yes/No; // Using Boolean datatype in Integer datatype
+      //(h)
+        int student_id; // Local variable not used
+      //(i)
+        int  A+3; // Syntax error
+      //(j)
+        int 'X'; // Using ' ' in variable declaration
+      //(k)
+        int return; // Using keywords in variabel declaration
+
+    }
+}
