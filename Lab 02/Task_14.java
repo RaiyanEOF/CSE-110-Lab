ab2afc8fcb738fb3955b120f2cbb8d6037da2b70
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-21 00:36:54 +0600

    Create Task_14.java

---
+++ Lab 02/Task_14.java
@@ -0,0 +1,10 @@
+public class Task14{
+    public static void main(String[] args){
+        double a = 8;  
+        double s = a; 
+        double circumference = 6*s;
+        double area = (3* Math.sqrt(3)/2)* Math.pow(s, 2);
+        System.out.println("Circumference: " + circumference);
+        System.out.println("Area: " + area);
+    }
+}
