bf226a41a2dabf1fe9126c3f20c1d4344a17ae0b
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-21 00:31:29 +0600

    Create Task_02.java

---
+++ Lab 02/Task_02.java
@@ -0,0 +1,41 @@
+public class Task02{
+    public static void main(String[] args){
+      //(2.1)
+        int a = 24301564;
+        System.out.println(a);
+      //(2.2)
+        int a = 24301564; int b = 124124;
+        int add = a+b;
+        System.out.println(add);
+      //(2.3)
+        int x = 15; int y = 3;
+        int Product = x*y;
+        System.out.println(Product);
+        int Division = x/y;
+        System.out.println(Division);
+      //(2.4)
+        double c = 1412.213;
+        System.out.println(c);
+        double c = 1412.213; double d = 13.232;
+        double Product = c*d;
+        double Division = c/d;
+        System.out.println(Product);
+        System.out.println(Division);
+      //(2.5)
+        int num1 = 10;
+        System.out.println(num1);
+        int num1 = 10; double num2 = 13.3;
+        double add = num1+num2;
+        System.out.println(add);
+        int num3 = 5; double num4 = 2.5;
+        double Product = num3*num4;
+        System.out.println(Product);
+        Double Division = num3/num2;
+        System.out.println(Division);
+      //(2.6) 
+        int Num1 = 1214;
+        String Num2 = 241241;
+        int Result = Num1+Num2;
+        System.out.println(Result); // Operation does not work because of different datatypes
+    }
+}
